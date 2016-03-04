88fc82a272eb4ee7a6210442366b3a45dbdcbb22
Kyle Oram <Kyle__oram@hotmail.com>
2016-03-04 16:38:25 -0500

    Teleporter nearly finalized
    Added scene detection and will show a message accordingly.

--- Alpha/Overclock 1.1a/Assets/Scripts/PortalScript.cs
+++ Alpha/Overclock 1.1a/Assets/Scripts/PortalScript.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class PortalScript : MonoBehaviour {
-
+	[SerializeField] private GameObject textDisplay;
 	private float timer = 0;
+	private bool goToRestArea = false;
+	private bool goToLevel2 = false;
 	private bool startTimer = false;
 	// Use this for initialization
 	void Start () {
@@ -15,16 +17,32 @@ public class PortalScript : MonoBehaviour {
 	void Update () {
 		if (startTimer == true)
 			timer += Time.deltaTime;
-		if (timer >= 2)
+		if (timer >= 2 && goToRestArea == true)
 			SceneManager.LoadScene ("Rest Area");
+		if (timer >= 2 && goToLevel2 == true)
+			SceneManager.LoadScene ("Level2");
 	}
+		
 	void OnTriggerStay2D(Collider2D col){
 		if (col.gameObject.tag == "Player") {
-			if (Input.GetKeyDown (KeyCode.T)) {
-				Debug.Log ("t");
+			if (Input.GetKeyDown (KeyCode.T) && SceneManager.GetActiveScene().name=="Level1") {
+				Debug.Log ("Pressed T");
+				goToRestArea = true;
 				startTimer = true;
-
+			}
+			if (Input.GetKeyDown (KeyCode.Y)) {
+				Debug.Log ("Pressed Y");
+				goToLevel2 = true;
+				startTimer = true;
 			}
 		}
+	}
+	void OnCollisionEnter2D (Collision2D col) {
+	if (col.gameObject.tag == "Player") 
+			textDisplay.SetActive (true);
+	}
+	void OnCollisionExit2D (Collision2D col) {
+		if (col.gameObject.tag == "Player") 
+			textDisplay.SetActive (false);
 	}
 }
--- Alpha/Overclock 1.1a/Assets/Scripts/gameMaster.cs
+++ Alpha/Overclock 1.1a/Assets/Scripts/gameMaster.cs
@@ -1,38 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class gameMaster : MonoBehaviour {
 
-	float overclockMax = 100f;
-	float overclockCur = 100f;
-	public bool overclock = false;
+
 
 	//public static void killPlayer (Player player){
 	//	killPlayer (player);
 //	}
 
 		void Start(){
-		InvokeRepeating ("TimeMechanic", 5, 5);
 		}
+}
 		
 
 
-		void TimeMechanic ()
-	{
-		if (Input.GetKeyDown (KeyCode.LeftShift) && overclockCur > 0) {
-			if (overclock) {
-				if (overclockCur > 0) {
-					overclockCur -= 5;
-					Time.timeScale = 0.7f;
-				}
-			} else {
-				if (overclockCur < overclockMax) {
-					overclockCur = +1;
-					overclock = false;
-					Time.timeScale = 1.0f;
-				}
-			}
-		}
-	}
-
-}
