6d2fac626d7906834b62acc4b689b1649d5255d0
Kyle Oram <Kyle__oram@hotmail.com>
2016-03-04 13:50:25 -0500

    Quality of Life Fix
    Since I didn't add any changelog from my previous pushes, I'll do them
    all here to update you guys on what I've done so far
    
    Weapons:
    -Added a functioning pistol, characteristics are as followed:
    - Character has an appropriate arm for the pistol
    - Shoot as you click the mouse button down
    - shoots as fast as you can click
    - 6 shots in a clip
    - Reload with R or when the weapon reaches 0
    - Orange trailing projectiles.
    Added a functioning automatic weapon named Blaster:
    - Character has an appropriate arm for the blaster
    - Shoots as you hold down the mouse button, 5 shots per second
    - 12 shots in a clip
    - Reload with R or when clip reaches 0 it will automatically reload,
    like the pistol
    - Blue trailing bullets
    Added a functioning grenade launcher:
    -Character is missing an arm sprite.
    -Shoots out a grenade as you click the mouse button
    - Grenade is affected by gravity and has a slight rotation for a few
    seconds
    - Grenade has an appropriate spite (Thanks to Pavel)
    *Work in Progress - the grenade doesn't do anything as of right now.
    also
    finalized values.
    Portal:
    -Functioning portal that when pressing 'T', the scene will change to the
    very unfinished Rest Area.
    -Added an appropriate sprite (Thanks again Pavel)
    *Work in Progress - needs an animation and some other things.
    
    Quality of Life Changes:
    -Major clean up to the hierarchy with subfolders for specific objects
    and scripts.
    -Added most of our GameObjects as prefabs so that switching scenes is
    actually possible.
    
    That's all what I can think of right now in terms of content and
    changes.
    
    As for a few bugs:
    - Arm needs to flip when aiming behind the player.

--- Alpha/Overclock 1.1a/Assets/Scripts/PortalScript.cs
+++ Alpha/Overclock 1.1a/Assets/Scripts/PortalScript.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class PortalScript : MonoBehaviour {
 
+	private float timer = 0;
+	private bool startTimer = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,14 +13,17 @@ public class PortalScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (startTimer == true)
+			timer += Time.deltaTime;
+		if (timer >= 2)
+			SceneManager.LoadScene ("Rest Area");
 	}
 	void OnTriggerStay2D(Collider2D col){
 		if (col.gameObject.tag == "Player") {
-			Debug.Log ("on");
 			if (Input.GetKeyDown (KeyCode.T)) {
-				Debug.Log ("t");	
-				SceneManager.LoadScene ("Rest Area");
+				Debug.Log ("t");
+				startTimer = true;
+
 			}
 		}
 	}
